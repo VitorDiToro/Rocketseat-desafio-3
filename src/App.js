625d456d3096330cdacc1deb1709ef9520674d10
VitorDiToro <vitorrditoro@gmail.com>
2020-10-29 16:32:26 -0300

    Vai teia!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react"
+import api from "./services/api"
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',{
+      title: 'bunda',
+      url: 'url',
+      techs: ['1', '2']
+    })
+
+    setRepositories([... repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+      setRepositories(repositories.filter(
+        repository => repository.id !== id 
+      ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.key}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
-  );
+  )
 }
 
 export default App;
